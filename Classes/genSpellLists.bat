c2e9d0beb04db7aee13997d45232581703f70e5a
DESKTOP-0PDEV4O\Ted <ted@tedneward.com>
2023-06-15 00:33:37 -0700

    Reformatting and refactoring

--- Classes/genSpellLists.bat
+++ Classes/genSpellLists.bat
@@ -1,12 +1,44 @@
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Artificer > Artificer\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Bard > Bard\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Cleric > Cleric\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Druid > Druid\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Paladin > Paladin\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd "Pale Master" > PaleMaster\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Ranger > Ranger\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Shaman > Shaman\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Sorcerer > Sorcerer\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Warlock > Warlock\SpellList.md
-python3 ..\Tools\spelltool.py --parsemd ..\Magic\Spells --listmd Wizard > Wizard\SpellList.md
+cd Artificer
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Artificer > SpellList.md
+cd ..
+
+cd Bard
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Bard > SpellList.md
+cd ..
+
+cd Cleric
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Cleric > SpellList.md
+cd ..
+
+cd Druid
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Druid > SpellList.md
+cd ..
+
+cd Paladin
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Paladin > SpellList.md
+cd ..
+
+cd PaleMaster
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd "Pale Master" > SpellList.md
+cd ..
+
+cd Ranger
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Ranger > SpellList.md
+cd ..
+
+cd Shaman
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Shaman > SpellList.md
+cd ..
+
+cd Sorcerer
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Sorcerer > SpellList.md
+cd ..
+
+cd Warlock
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Warlock > SpellList.md
+cd ..
+
+cd Wizard
+python3 ../../Tools/spelltool.py --parsemd ../../Magic/Spells --listmd Wizard > SpellList.md
+cd ..
 
