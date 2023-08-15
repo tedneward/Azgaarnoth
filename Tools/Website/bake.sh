1c3064a67d4238b27a0fafec26bc114ddb42ac8b
Ted Neward <ted@tedneward.com>
2023-08-14 23:08:26 -0700

    Website gen incorporated into Tools

---
+++ Tools/Website/bake.sh
@@ -0,0 +1,28 @@
+# Clean up from previous runs, if any
+rm -r docs
+rm -r site
+
+# Copy content into `docs`
+mkdir docs
+for i in ../../*
+do
+  if [ $i != "../../Tools" ] && [ $i != "../../Supplements" ]
+  then
+    if [ -d $i ]
+    then
+      # Get directory name out of i, mkdir it in docs
+      DIR="$(basename $i)"
+      echo $DIR
+      mkdir docs/$DIR
+      cp -r $i/* docs/$DIR
+    else
+      cp $i docs
+    fi
+  fi
+done
+
+# Convert it
+mkdocs build
+
+# Upload it (via Dropbox)
+cp -r site/* ~/Dropbox/Apps/site44/azgaarnoth.tedneward.com/
