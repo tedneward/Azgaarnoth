89f01a5081fa08f273ca369208cb188b79903469
DESKTOP-0PDEV4O\Ted <ted@tedneward.com>
2023-08-15 19:51:14 -0700

    Creatool mods and sample parsing targets

--- Tools/creatool.py
+++ Tools/creatool.py
@@ -9,12 +9,16 @@ import os
 import sqlite3
 import sys
 
+# These sorts of creature entries are typically for a collection of such; the
+# "Troll" entry, for example, contains "Troll", "Aquatic Troll", "Rot Troll",
+# and so on.
 class SubtypedCreature:
      def __init__(self):
         self.name = ''
         self.subtypes = []
         self.generaldescription = ''
 
+# A creature entry.
 class Creature:
     class Size(Enum):
         TINY = 'Tiny',
@@ -61,6 +65,12 @@ class Creature:
         self.bonusactions = []
         self.reactions = []
 
+    def abilitybonus(self, score):
+        return (score / 2) - 5
+
+    def abilitytext(self, score):
+        return f"{score} ({self.abilitybonus(score):+g})"
+
     def parseMD(self, mdlines):
         "Parse a list of strings that contain well-formed Markdown"
         pass
@@ -70,11 +80,13 @@ class Creature:
         pass
 
     def emitMD(self):
+        "Emit this creature description and stat block"
+
         linebreak = ">___\n"
 
-        "Emit this creature description and stat block"
-        result  = f"## {self.name}"
-        result += "\n\n".join(self.description)
+        result  = ""
+        #result += f"## {self.name}"
+        #result += "\n\n".join(self.description)
         result += f">### {self.name}\n"
         result += f">*{self.size} {self.type}, {self.alignment}*"
         result += linebreak
@@ -84,7 +96,8 @@ class Creature:
         result += linebreak
         result +=  ">|**STR**|**DEX**|**CON**|**INT**|**WIS**|**CHA**|\n"
         result +=  ">|:---:|:---:|:---:|:---:|:---:|:---:|\n"
-        result += f">|10 (0)|10 (0)|10 (0)|10 (+0)|10 (+0)|10 (0)|\n"
+        result += f">|{self.abilitytextself.STR}|{self.abilitytextself.DEX}|{self.abilitytextself.CON}|"
+        result += f"{self.abilitytextself.INT}|{self.abilitytextself.WIS}|{self.abilitytextself.CHA}|\n"
         result +=  ">\n"
         result += linebreak
         result += f">- **Proficiency Bonus** {self.profbonus}\n"
@@ -136,6 +149,7 @@ def main(argv):
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     parser.add_argument('--parsemd', help='Source directory of MD files to use')
     parser.add_argument('--parsesql', help='Source SQLite database to use')
+    parser.add_argument('--testmd', help='Use test Creature')
     parser.add_argument('--ingest', help='File(s) to import into the set')
     parser.add_argument('--lint', help='Warn about non-normalized data')
     #parser.add_argument('--filter', help='Filter the source set') # When I figure out an expression language to use
@@ -144,19 +158,33 @@ def main(argv):
     parser.add_argument('--writemd', help='Target directory for MD files to be emitted')
     args = parser.parse_args()
 
+    creatures = []
     if args.parsemd != None:
         # Parse MD files
         pass
     elif args.parsesql != None:
         # Parse SQLite db
         pass
+    elif args.testmd != None:
+        creature = Creature()
+        creature.name = 'Testit'
+        creature.size = Creature.Size.MEDIUM
+        creature.type = Creature.Type.FEY
+        creature.alignment = 'neutral'
+        creature.ac = '15 (natural armor)'
+        creature.hp = '135 (12d10 + 35)'
     else:
         print("No source set specified!")
 
+    # Filter?
+
+    # Ingest?
     if args.ingest != None:
         ingest(args.ingest)
     else:
         parser.print_help()
+
+    # Store?
 
 if __name__ == '__main__':
 	main(sys.argv)
