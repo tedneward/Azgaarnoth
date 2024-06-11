3486cd6cd7611ba85268dcff9fb090102f2200c6
Ted Neward <ted@tedneward.com>
2024-06-11 01:41:31 -0700

    Some more menu refactoring

--- Tools/Website/genCreatureLists.sh
+++ Tools/Website/genCreatureLists.sh
@@ -13,5 +13,37 @@
 ../creatool.py --parsemd ../../Creatures --filtertype ooze --writeindex docs/Creatures/index-ooze.md
 ../creatool.py --parsemd ../../Creatures --filtertype plant --writeindex docs/Creatures/index-plant.md
 ../creatool.py --parsemd ../../Creatures --filtertype undead --writeindex docs/Creatures/index-undead.md
+
+../creatool.py --parsemd ../../Creatures --filterenv Arctic --writeindex docs/Creatures/index-arctic.md
+../creatool.py --parsemd ../../Creatures --filterenv Coastal --writeindex docs/Creatures/index-coastal.md
+../creatool.py --parsemd ../../Creatures --filterenv Desert  --writeindex docs/Creatures/index-desert.md
+../creatool.py --parsemd ../../Creatures --filterenv Forest --writeindex docs/Creatures/index-forest.md
+../creatool.py --parsemd ../../Creatures --filterenv Grassland --writeindex docs/Creatures/index-grassland.md
+../creatool.py --parsemd ../../Creatures --filterenv Hill --writeindex docs/Creatures/index-hill.md
+../creatool.py --parsemd ../../Creatures --filterenv Mountain --writeindex docs/Creatures/index-mountain.md
+../creatool.py --parsemd ../../Creatures --filterenv Ocean --writeindex docs/Creatures/index-ocean.md
+../creatool.py --parsemd ../../Creatures --filterenv Swamp --writeindex docs/Creatures/index-swamp.md
+../creatool.py --parsemd ../../Creatures --filterenv Underdark --writeindex docs/Creatures/index-underdark.md
+../creatool.py --parsemd ../../Creatures --filterenv Underwater --writeindex docs/Creatures/index-underwater.md
+../creatool.py --parsemd ../../Creatures --filterenv Urban --writeindex docs/Creatures/index-urban.md
+
+../creatool.py --parsemd ../../Creatures --filtercr 1/4 --writeindex docs/Creatures/index-cr1-4.md
+../creatool.py --parsemd ../../Creatures --filtercr 1/2 --writeindex docs/Creatures/index-cr1-2.md
+../creatool.py --parsemd ../../Creatures --filtercr 1 --writeindex docs/Creatures/index-cr1.md
+../creatool.py --parsemd ../../Creatures --filtercr 2 --writeindex docs/Creatures/index-cr2.md
+../creatool.py --parsemd ../../Creatures --filtercr 3 --writeindex docs/Creatures/index-cr3.md
+../creatool.py --parsemd ../../Creatures --filtercr 4 --writeindex docs/Creatures/index-cr4.md
+../creatool.py --parsemd ../../Creatures --filtercr 5 --writeindex docs/Creatures/index-cr5.md
+../creatool.py --parsemd ../../Creatures --filtercr 6 --writeindex docs/Creatures/index-cr6.md
+../creatool.py --parsemd ../../Creatures --filtercr 7 --writeindex docs/Creatures/index-cr7.md
+../creatool.py --parsemd ../../Creatures --filtercr 8 --writeindex docs/Creatures/index-cr8.md
+../creatool.py --parsemd ../../Creatures --filtercr 9 --writeindex docs/Creatures/index-cr9.md
+../creatool.py --parsemd ../../Creatures --filtercr 10 --writeindex docs/Creatures/index-cr10.md
+../creatool.py --parsemd ../../Creatures --filtercr 11 --writeindex docs/Creatures/index-cr11.md
+../creatool.py --parsemd ../../Creatures --filtercr 12 --writeindex docs/Creatures/index-cr12.md
+../creatool.py --parsemd ../../Creatures --filtercr 13 --writeindex docs/Creatures/index-cr13.md
+../creatool.py --parsemd ../../Creatures --filtercr 14 --writeindex docs/Creatures/index-cr14.md
+../creatool.py --parsemd ../../Creatures --filtercr 15 --writeindex docs/Creatures/index-cr15.md
+
 ../creatool.py --parsemd ../../Creatures --writeindex docs/Creatures/index-az.md
 
